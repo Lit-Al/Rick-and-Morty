3bc8d1e4a768813efbda578fab9f8cba54044491
Lit-Al <93156911+Lit-Al@users.noreply.github.com>
2023-06-12 14:37:21 +0300

    Update episodes.js

--- js/episodes.js
+++ js/episodes.js
@@ -21,13 +21,26 @@ import {getResultsApi} from './utils/getDataApi.js';
   };
   makeCards(dataEpisodes);
   const moreBtn = document.querySelector(".more-btn");
+  const filterInp = document.querySelector(".filter-input");
   let count = 2;
   moreBtn.addEventListener("click", async () => {
+    filterInp.value = null;
+    const categories = document.querySelectorAll('.section-container')
+    for (const category of categories) {
+      const cardsList = category.querySelector('.cards-list')
+      const cardsItem = cardsList.getElementsByTagName('li')
+      for (let i = 0; i < cardsItem.length; i++) {
+      cardsItem[i].style.display = "flex"
+      }
+    }
+    
     const makeMore = async (Arr) => {
-      Arr = await getResultsApi.getData(API_URL + URL_EPISODE + "/?page=" + count);
+      Arr = await getResultsApi.getData(
+        API_URL + URL_EPISODE + "/?page=" + count
+      );
       makeCards(Arr);
     };
     makeMore(dataEpisodes);
-    count++;
+    count++;               
   });
 })();
